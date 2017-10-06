174c37340873a41fa33cf666db07969629f5957c
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-10-05 19:20:06 -0500

    archivos base

---
+++ PATS-32-5-adivinaNuero0-100/src/pats/pkg32/pkg5/adivinanuero0/pkg100/PATS325AdivinaNuero0100.java
@@ -0,0 +1,22 @@
+/*
+Ejercicio 5: Realizar un juego para adivinar un numero. Para ello genera un 
+número aleatorio entre 0-100 y luego ir pidiendo números indicando "es mayor" o 
+"es menor" según sea el caso. El proceso termina cuando el usuario acierta y 
+mostrar el número de intentos.
+ */
+package pats.pkg32.pkg5.adivinanuero0.pkg100;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS325AdivinaNuero0100 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
